7d5414cbe7a5f8ee26c12462f04a81785c1a607a
Rahul <rg4005450@gmail.com>
2023-04-30 13:00:09 +0530

    Changed to function based component

--- src/Components/error.js
+++ src/Components/error.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import errorImg from "./error.png"
-export default class error extends Component {
-    reload = () => {
+
+function error(props) {
+    const reload = () => {
         location.reload();
     }
-    render() {
-        let mode = this.props;
-        return (
+    let mode = props.mode;
+    return (
+        <>
             <div className='container'>
                 <img src={errorImg} alt="error" />
                 <h1 className='my-5'><b><i>Error</i></b> occured while fetching news</h1>
                 <p>Try:</p>
                 <li>Checking the connection</li>
                 <li>Reconnecting to Wi-Fi</li>
-                <button className={`btn btn-lg my-5 btn-${mode === 'dark' ? 'light' : 'dark'} bg-${mode === 'dark' ? 'dark' : 'light'} text-${mode === 'dark' ? 'light' : 'dark'}`} onClick={this.reload}>Try Again</button>
+                <button className={`btn btn-lg my-5 btn-${mode === 'dark' ? 'light' : 'dark'} bg-${mode === 'dark' ? 'dark' : 'light'} text-${mode === 'dark' ? 'light' : 'dark'}`} onClick={reload}>Try Again</button>
             </div>
-        )
-    }
+        </>
+    )
 }
+
+export default error
