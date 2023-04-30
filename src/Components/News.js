92ea30c1b0fe917d0d3e9a6b93770ba72ab59094
Rahul <rg4005450@gmail.com>
2023-04-30 13:42:08 +0530

    Change to function based component

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,147 +1,145 @@
-import React, { Component } from "react";
+import React from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./spinner";
 import Error from "./error";
 import PropTypes from "prop-types";
-import logo from "./logo.jpg"
-export class News extends Component {
-  articles = [];
-  static defaultProps = {
-    country: "in",
-    category: "general",
-    pageSize: 15,
-  };
-  static propTypes = {
-    country: PropTypes.string,
-    category: PropTypes.string,
-    pageSize: PropTypes.number,
-  };
-  capitalizeFirstLetter = (string) => {
+import logo from "./logo.jpg";
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+export default function News(props) {
+  const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: this.articles,
-      page: 1,
-      loader: false,
-      error: false,
-    };
-    document.title = `${this.capitalizeFirstLetter(
-      this.props.category
-    )} - NewsMonkey`;
-  }
+  document.title = `${capitalizeFirstLetter(
+    props.category
+  )} - NewsMonkey`;
 
-  async updateNews(pageNo) {
+  // Defining States
+  const [articles, setArticles] = useState([])
+  const [page, setPage] = useState(1);
+  const [loader, setLoader] = useState(false)
+  const [error, setError] = useState(false)
+  const [totalResults, setTotalResults] = useState(0)
+
+  const updateNews = async (pageNo) => {
     try {
-      this.props.setProgress(0);
-      this.setState({ loader: true, error: false });
-      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${pageNo}&pageSize=${this.props.pageSize}`;
-      this.props.setProgress(50);
+      props.setProgress(0);
+      setError(false);
+      setLoader(true)
+      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${pageNo}&pageSize=${props.pageSize}`;
+      props.setProgress(50);
       const res = await fetch(url);
       const data = await res.json();
-      this.setState({
-        articles: data.articles,
-        totalResults: data.totalResults,
-        loader: false,
-      });
-      this.props.setProgress(100);
+      setArticles(data.articles)
+      setTotalResults(data.totalResults)
+      setLoader(false)
+      props.setProgress(100);
     } catch (e) {
-      this.props.setProgress(0);
-      this.setState({ error: true, loader: false });
+      props.setProgress(0);
+      setError(true);
+      setLoader(false)
       console.log(new Error("Fail to fetch news"));
-      this.props.setProgress(100);
+      props.setProgress(100);
     }
   }
 
-  async componentDidMount() {
-    await this.updateNews(1);
-  }
-
-  handlePrevious = async () => {
-    this.setState({ page: this.state.page - 1 });
-    let pageNO = this.state.page - 1;
-    this.updateNews(pageNO);
+  const handlePrevious = async () => {
+    setPage(page - 1)
+    let pageNO = page - 1;
+    await updateNews(pageNO);
   };
 
-  handleNext = async () => {
-    this.setState({ page: this.state.page + 1 });
-    let pageNO = this.state.page + 1;
-    this.updateNews(pageNO);
+  const handleNext = async () => {
+    setPage(page + 1)
+    let pageNO = page + 1;
+    await updateNews(pageNO);
   };
 
-  render() {
-    return (
-      <div className="container">
-        <h2 className="text-center" style={{ marginTop: "70px" }}>
-          News Monkey Top {this.capitalizeFirstLetter(this.props.category)}{" "}
-          Headliness
-        </h2>
-        {this.state.error && <Error mode={this.props.mode} />}
-        {this.state.loader && <Spinner mode={this.props.mode} />}
-        <div className="row my-5">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                {this.state.error === false && this.state.loader === false ? (
-                  <NewsItem
-                    title={element.title}
-                    description={element.description}
-                    mode={this.props.mode}
-                    imageUrl={
-                      element.urlToImage != null
-                        ? element.urlToImage
-                        : logo
-                    }
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt.replace(/T|Z/g, " ")}
-                    source={element.source.name}
-                  />
-                ) : (
-                  ""
-                )}
-              </div>
-            );
-          })}
-        </div>
-        {this.state.error === false ? (
-          <div className="container d-flex justify-content-between">
-            <button
-              type="button"
-              disabled={this.state.page <= 1}
-              className={`btn btn-${
-                this.props.mode === "light" ? "dark" : "light"
-              } bg-${this.props.mode === "light" ? "light" : "dark"} text-${
-                this.props.mode === "light" ? "dark" : "light"
-              }`}
-              onClick={this.handlePrevious}
-            >
-              &larr; Previous
-            </button>
-            <button
-              type="button"
-              id="nextBTN"
-              disabled={
-                this.state.page + 1 >
-                Math.ceil(this.state.totalResults / this.props.pageSize)
-              }
-              className={`btn btn-${
-                this.props.mode === "light" ? "dark" : "light"
-              } bg-${this.props.mode === "light" ? "light" : "dark"} text-${
-                this.props.mode === "light" ? "dark" : "light"
-              }`}
-              onClick={this.handleNext}
-            >
-              Next &rarr;
-            </button>
-          </div>
-        ) : (
-          ""
-        )}
+  useEffect(() => {
+    updateNews(1)
+  }, [])
+
+  return (
+    <div className="container">
+      <h2 className="text-center" style={{ marginTop: "70px" }}>
+        News Monkey Top {capitalizeFirstLetter(props.category)}{" "}
+        Headliness
+      </h2>
+      {error && <Error mode={props.mode} />}
+      {loader && <Spinner mode={props.mode} />}
+      <div className="row my-5">
+        {articles.map((element) => {
+          return (
+            <div className="col-md-4" key={element.url}>
+              {error === false && loader === false ? (
+                <NewsItem
+                  title={element.title}
+                  description={element.description}
+                  mode={props.mode}
+                  imageUrl={
+                    element.urlToImage != null ? element.urlToImage : logo
+                  }
+                  newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt.replace(/T|Z/g, " ")}
+                  source={element.source.name}
+                />
+              ) : (
+                ""
+              )}
+            </div>
+          );
+        })}
       </div>
-    );
-  }
+      {error === false ? (
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            disabled={page <= 1}
+            className={`btn btn-${props.mode === "light" ? "dark" : "light"
+              } bg-${props.mode === "light" ? "light" : "dark"} text-${props.mode === "light" ? "dark" : "light"
+              }`}
+            onClick={handlePrevious}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            id="nextBTN"
+            disabled={
+              page + 1 >
+              Math.ceil(totalResults / props.pageSize)
+            }
+            className={`btn btn-${props.mode === "light" ? "dark" : "light"
+              } bg-${props.mode === "light" ? "light" : "dark"} text-${props.mode === "light" ? "dark" : "light"
+              }`}
+            onClick={handleNext}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
-export default News;
+// Default props
+News.defaultProps = {
+  country: "in",
+  category: "general",
+  pageSize: 15,
+};
+
+// Proptypes
+News.propTypes = {
+  country: PropTypes.string,
+  category: PropTypes.string,
+  pageSize: PropTypes.number,
+};
+
+
+
