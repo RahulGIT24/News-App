2c08c16ec69c55b5fb0ce7e5b7e69cd5599ea248
Rahul <rg4005450@gmail.com>
2023-05-01 14:53:03 +0530

    Changed it to function based component

--- src/App.js
+++ src/App.js
@@ -1,61 +1,53 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar'
 import News from './Components/News'
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
-//* Class based component
-export default class App extends Component {
-  pageSize = 15;
-  apiKey = process.env.REACT_APP_NEWS_API
-  constructor(props) {
-    super(props);
-    this.state = {
-      mode: 'light'
-    }
-  }
-  toogleBtn = () => {
-    if (this.state.mode === 'light') {
-      this.setState({
-        mode: 'dark'
-      })
+function App() {
+  const pageSize = 15;
+  const apiKey = process.env.REACT_APP_NEWS_API
+
+  // Defining state
+  const [mode, setMode] = useState('light')
+  const [progress, setProgres] = useState(0)
+
+  const toogleBtn = () => {
+    if (mode === 'light') {
+      setMode('dark')
       document.body.style.backgroundColor = '#212529'
       document.body.style.color = 'white'
     } else {
-      this.setState({
-        mode: 'light'
-      })
+      setMode('light')
       document.body.style.backgroundColor = 'white'
       document.body.style.color = '#212529'
     }
   }
-  state = {
-    progress: 0
+
+  const setProgress = (progress) => {
+    setProgres(progress)
   }
-  setProgress = (progress) => {
-    this.setState({ progress: progress })
-  }
-  render() {
-    return (
-      <Router>
-        <Navbar toogleBtn={this.toogleBtn} mode={this.state.mode} />
-        <LoadingBar
-          color='#f11946'
-          height={3}
-          progress={this.state.progress}
-        />
-        <Routes>
-          <Route exact path="/" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key="home" pageSize={this.pageSize} mode={this.state.mode} />} />
-          <Route exact path="/sports" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key='sports' category='sports' pageSize={this.pageSize} mode={this.state.mode} />} />
-          <Route exact path="/business" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key='business' category='business' pageSize={this.pageSize} mode={this.state.mode} />} />
-          <Route exact path="/entertainment" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key='entertainment' category='entertainment' pageSize={this.pageSize} mode={this.state.mode} />} />
-          <Route exact path="/general" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key='general' category='general' pageSize={this.pageSize} mode={this.state.mode} />} />
-          <Route exact path="/health" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key='health' category='health' pageSize={this.pageSize} mode={this.state.mode} />} />
-          <Route exact path="/science" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key='science' category='science' pageSize={this.pageSize} mode={this.state.mode} />} />
-          <Route exact path="/technology" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key='technology' category='technology' pageSize={this.pageSize} mode={this.state.mode} />} />
-        </Routes>
-      </Router>
-    )
-  }
+
+  return (
+    <Router>
+      <Navbar toogleBtn={toogleBtn} mode={mode} />
+      <LoadingBar
+        color='#f11946'
+        height={3}
+        progress={progress}
+      />
+      <Routes>
+        <Route exact path="/" element={<News apiKey={apiKey} setProgress={setProgress} key="home" pageSize={pageSize} mode={mode} />} />
+        <Route exact path="/sports" element={<News apiKey={apiKey} setProgress={setProgress} key='sports' category='sports' pageSize={pageSize} mode={mode} />} />
+        <Route exact path="/business" element={<News apiKey={apiKey} setProgress={setProgress} key='business' category='business' pageSize={pageSize} mode={mode} />} />
+        <Route exact path="/entertainment" element={<News apiKey={apiKey} setProgress={setProgress} key='entertainment' category='entertainment' pageSize={pageSize} mode={mode} />} />
+        <Route exact path="/general" element={<News apiKey={apiKey} setProgress={setProgress} key='general' category='general' pageSize={pageSize} mode={mode} />} />
+        <Route exact path="/health" element={<News apiKey={apiKey} setProgress={setProgress} key='health' category='health' pageSize={pageSize} mode={mode} />} />
+        <Route exact path="/science" element={<News apiKey={apiKey} setProgress={setProgress} key='science' category='science' pageSize={pageSize} mode={mode} />} />
+        <Route exact path="/technology" element={<News apiKey={apiKey} setProgress={setProgress} key='technology' category='technology' pageSize={pageSize} mode={mode} />} />
+      </Routes>
+    </Router>
+  )
 }
 
+export default App
